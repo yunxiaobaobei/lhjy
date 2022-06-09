3df3c6ef89a8bf772923293ab29a3ba75815f0bb
niexiong <2461056952@qq.com>
2022-06-09 18:33:54 +0800

    优化

---
+++ StockTest/StockTest/Common.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.IO;
+
+namespace StockTest
+{
+    internal class Common
+    {
+        public static string ConvertJsonString(string str)
+        {
+            try
+            {
+                //格式化json字符串
+                JsonSerializer serializer = new JsonSerializer();
+                TextReader tr = new StringReader(str);
+                JsonTextReader jtr = new JsonTextReader(tr);
+                object obj = serializer.Deserialize(jtr);
+                if (obj != null)
+                {
+                    StringWriter textWriter = new StringWriter();
+                    JsonTextWriter jsonWriter = new JsonTextWriter(textWriter)
+                    {
+                        Formatting = Formatting.Indented,
+                        Indentation = 4,
+                        IndentChar = ' '
+                    };
+                    serializer.Serialize(jsonWriter, obj);
+                    return textWriter.ToString();
+                }
+
+                return str;
+            }
+            catch (Exception ex)
+            {
+                return null;
+            }
+        }
+
+
+    }
+}
---
+++ StockTest/StockTest/StockInfo.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+namespace StockTest
+{
+    public class StockInfo
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string Exchange { get; set; }
+        public string Abbreviation { get; set; }
+
+    }
+
+    public class Record
+    {
+        [JsonProperty("RECORDS")]
+        public List<StockInfo> stockInfos { get; set; }
+    }
+
+}
