997e9b90a4596c1b58bc9391ce8fa00c45f613cc
niexiong <2461056952@qq.com>
2022-07-02 14:00:26 +0800

    添加配置类

---
+++ StockTest/StockTest/ColorConfig.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+namespace StockTest
+{
+    public class ColorConfig
+    {
+        #region 颜色配置
+        public Color BuyInColor { get; set; }
+
+        public Color SellOutColor { get; set; }
+
+        public Color HeigVolumnColor { get; set; }
+
+        public Color LowVolumnColor { get; set; }
+
+        public Color EnterPointColor { get; set; }
+
+        public Color ExitPointColor { get; set; }
+        #endregion
+
+
+        #region 是否启用相关颜色
+        public bool AbleBuy { get; set; }
+
+
+        public bool AbleSell{ get; set; }
+
+        public bool AbleHeigVolumn { get; set; }
+
+        public bool AbleLowVolumn { get; set; }
+
+        public bool AbleEnterPoint { get; set; }
+
+        public bool AbleExitPoint { get; set; }
+        #endregion
+    }
+}
