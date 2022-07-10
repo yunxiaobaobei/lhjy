80f233bea2035eb7a08a930828e73dedc1e4569c
niexiong <2461056952@qq.com>
2022-07-10 16:31:41 +0800

    优化

---
+++ StockTest/AutoDeal/model/sina/Minute.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace AutoDeal.model.sina
+{
+    public class Minute
+    {
+        [JsonProperty("day")]
+        public DateTime Date
+        {
+            get;
+            set;
+        }
+
+        public decimal Open
+        {
+            get;
+            set;
+        }
+
+        public decimal High
+        {
+            get;
+            set;
+        }
+
+        public decimal Low
+        {
+            get;
+            set;
+        }
+
+        public decimal Close
+        {
+            get;
+            set;
+        }
+
+        public decimal Volume
+        {
+            get;
+            set;
+        }
+
+
+    }
+}
